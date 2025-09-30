894f2896e206e5af803514ce5b65534a553d1102
Jithin Philip <jithinphilip347@gmail.com>
2025-09-30 20:24:26 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../../my-app/src/assets/css/main.css'
-import '../../my-app/src/assets/css/style.css'
+import './assets/css/main.css'
+import './assets/css/style.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
